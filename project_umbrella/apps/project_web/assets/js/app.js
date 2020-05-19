7f38cd2a7984bf2ce6e3b5de31ab4ee3aecd6482
Rafael Polfliet <rafael.polfliet@student.ucll.be>
2020-05-20 00:45:39 +0200

    javascript fix

--- project_umbrella/apps/project_web/assets/js/app.js
+++ project_umbrella/apps/project_web/assets/js/app.js
@@ -16,20 +16,3 @@ import "phoenix_html"
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
 
-window.onload = startTime
-
-function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('clock').innerHTML =
-    h + ":" + m + ":" + s;
-    var t = setTimeout(startTime, 500);
-  }
-  function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-  }
--- project_umbrella/apps/project_web/assets/webpack.config.js
+++ project_umbrella/apps/project_web/assets/webpack.config.js
@@ -13,10 +13,12 @@ module.exports = (env, options) => ({
     ]
   },
   entry: {
-    './js/app.js': glob.sync('./vendor/**/*.js').concat(['./js/app.js'])
+    'app.js': glob.sync('./vendor/**/*.js').concat(['./js/app.js']),
+    'clock.js': ['./js/clock.js'].concat(glob.sync('./vendor/**/*.js'))
+
   },
   output: {
-    filename: 'app.js',
+    filename: '[name]',
     path: path.resolve(__dirname, '../priv/static/js')
   },
   module: {
